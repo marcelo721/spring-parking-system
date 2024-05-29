ad57e4b41d70bb1d0862f157af706bb01e01a82b
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-29 10:00:49 -0300

    configured timezone and locale

---
+++ src/main/java/com/marceloHsousa/demo_part_api/config/SpringTimeZoneConfig.java
@@ -0,0 +1,16 @@
+package com.marceloHsousa.demo_part_api.config;
+
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.TimeZone;
+
+@Configuration
+public class SpringTimeZoneConfig {
+
+    @PostConstruct
+    public void timeZoneConfig(){
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_paulo"));
+    }
+}
---
+++ src/test/java/com/marceloHsousa/demo_part_api/DemoPartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marceloHsousa.demo_part_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPartApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
