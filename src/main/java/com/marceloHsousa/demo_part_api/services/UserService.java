2551b957e35a26f09c12631b6bac002437e42c9f
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-29 13:42:21 -0300

    created method insert in UserController and UserService class

--- src/main/java/com/marceloHsousa/demo_part_api/services/UserService.java
+++ src/main/java/com/marceloHsousa/demo_part_api/services/UserService.java
@@ -1,13 +1,20 @@
 package com.marceloHsousa.demo_part_api.services;
 
 
+import com.marceloHsousa.demo_part_api.entities.User;
 import com.marceloHsousa.demo_part_api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public User insert(User user){
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/marceloHsousa/demo_part_api/web/controller/UserController.java
+++ src/main/java/com/marceloHsousa/demo_part_api/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.marceloHsousa.demo_part_api.web.controller;
 
+import com.marceloHsousa.demo_part_api.entities.User;
 import com.marceloHsousa.demo_part_api.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService service;
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
