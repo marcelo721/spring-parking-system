f530e41013d31d048e35e2a75b2a3ca8c5bb6717
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-05-31 14:02:20 -0300

    created UserDto class

---
+++ src/main/java/com/marceloHsousa/demo_part_api/web/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.marceloHsousa.demo_part_api.web.dto;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class UserDto {
+
+    private String username;
+    private String password;
+}
