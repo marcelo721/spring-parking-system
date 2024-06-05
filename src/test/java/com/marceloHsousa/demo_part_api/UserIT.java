7a92d545e7abd662548d0158ea72695dabc30fdc
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-06-05 16:46:45 -0300

    created createUser_withValidData_returnsUserstatus201 method test

--- src/test/java/com/marceloHsousa/demo_part_api/UserIT.java
+++ src/test/java/com/marceloHsousa/demo_part_api/UserIT.java
@@ -1,17 +1,40 @@
 package com.marceloHsousa.demo_part_api;
 
 
+import com.marceloHsousa.demo_part_api.web.dto.UserDto;
+import com.marceloHsousa.demo_part_api.web.dto.UserResponseDto;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql(scripts = "sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(scripts = "sql/users/users-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class UserIT {
 
     @Autowired
     WebTestClient testClient;
+
+    @Test
+    public void createUser_withValidData_returnsUserstatus201(){
+
+        UserResponseDto responseBody = testClient
+                .post()
+                .uri("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserDto("joao@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UserResponseDto.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getId()).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getUsername()).isEqualTo("joao@gmail.com");
+        org.assertj.core.api.Assertions.assertThat(responseBody.getRole()).isEqualTo("CLIENT");
+    }
 
 }
