65f16f52db307a65a0df7882d8e9f7faa7f95e76
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-06-05 15:09:48 -0300

    Started the creation of the test layer

---
+++ src/test/java/com/marceloHsousa/demo_part_api/resources/sql/users/users-insert.sql
@@ -0,0 +1,3 @@
+insert into users (id, username, password, role) value(100, 'marcelo@gmail.com','123456', 'ROLE_ADMIN');
+insert into users (id, username, password, role) value(101, 'LUCAS@gmail.com','123456', 'ROLE_ADMIN');
+insert into users (id, username, password, role) value(102, 'JOAO@gmail.com','123456', 'ROLE_ADMIN');
