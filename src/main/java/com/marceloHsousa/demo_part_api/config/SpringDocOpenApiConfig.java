b1a0c2a61cbbe1ec55f330a8554c522423bcdd0b
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-06-05 14:07:03 -0300

    added SpringDocOpenApiConfig class in config package

---
+++ src/main/java/com/marceloHsousa/demo_part_api/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.marceloHsousa.demo_part_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+
+        return new OpenAPI().info(
+                new Info()
+                        .title("Rest Api - Spring Park")
+                        .description("api for vehicle parking management")
+                        .version("v1")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                        .contact(new Contact().name("Marcelo Henrique de Sousa").email("mh0473569@gmail.com"))
+        );
+    }
+}
