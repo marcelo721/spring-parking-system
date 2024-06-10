d86dccbd4d69831aa08559cd6cfa7ae76645dc56
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-06-10 08:54:48 -0300

    created JwtToken class and JwtUtils class in jwt package

--- src/main/java/com/marceloHsousa/demo_part_api/config/SpringSecurityConfig.java
+++ src/main/java/com/marceloHsousa/demo_part_api/config/SpringSecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,7 +27,7 @@ public class SpringSecurityConfig {
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(
-                        auth -> auth.requestMatchers(HttpMethod.POST, "api/v1/users").permitAll()
+                        auth -> auth.requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
                                 .anyRequest().authenticated()
                 ).sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
---
+++ src/main/java/com/marceloHsousa/demo_part_api/jwt/JwtUtils.java
@@ -0,0 +1,105 @@
+package com.marceloHsousa.demo_part_api.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Slf4j
+public class JwtUtils {
+
+    public static final String JWT_BEARER = "bearer";
+
+    public static final String JWT_AUTHORIZATION = "authorization";
+
+    public static final String SECRET_KEY = "0123456789-0123456789-0123456789";
+
+    public static final long  EXPIRE_DAYS = 0;
+
+    public static final long  EXPIRE_HOURS = 0;
+
+    public static final long  EXPIRE_MINUTES = 2;
+
+
+    private JwtUtils(){
+    }
+
+    private static Key generateKey(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static Date toExpireDate(Date start){
+
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS).plusHours(EXPIRE_HOURS).plusMinutes(EXPIRE_MINUTES);
+
+        return Date.from(end.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static JwtToken createToken(String username, String role){
+
+        Date issueAt = new Date();
+        Date limit = toExpireDate(issueAt);
+
+        String token = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setSubject(username)
+                .setIssuedAt(issueAt)
+                .setExpiration(limit)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .claim("role", role)
+                .compact();
+
+        return new JwtToken(token);
+
+    }
+
+    private static Claims getClaimsFromToken(String token){
+
+        try {
+
+            return Jwts.parserBuilder()
+                    .setSigningKey(generateKey()).build()
+                    .parseClaimsJws(refactorToken(token)).getBody();
+
+        }catch (JwtException e){
+            log.error(String.format("Invalid token", e.getMessage()));
+        }
+        return null;
+    }
+
+    private static String refactorToken(String token){
+
+        if(token.contains(JWT_BEARER)){
+            return token.substring(JWT_BEARER.length());
+        }
+        return token;
+    }
+
+    public static String getUsernameFromToken(String token){
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token){
+
+        try {
+
+             Jwts.parserBuilder()
+                    .setSigningKey(generateKey()).build()
+                    .parseClaimsJws(refactorToken(token));
+             return true;
+
+        }catch (JwtException e){
+            log.error(String.format("Invalid token", e.getMessage()));
+        }
+        return false;
+    }
+}
