e068329f791c7235d1f59be27fe0e721e29563b8
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-06-13 13:55:09 -0300

    Implemented ClientController documentation

--- src/main/java/com/marceloHsousa/demo_part_api/web/controller/ClientController.java
+++ src/main/java/com/marceloHsousa/demo_part_api/web/controller/ClientController.java
@@ -7,6 +7,12 @@ import com.marceloHsousa.demo_part_api.services.UserService;
 import com.marceloHsousa.demo_part_api.web.dto.clientDTO.ClientCreateDto;
 import com.marceloHsousa.demo_part_api.web.dto.clientDTO.ClientResponseDto;
 import com.marceloHsousa.demo_part_api.web.dto.mapper.ClientMapper;
+import com.marceloHsousa.demo_part_api.web.exceptions.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Clients", description = "this class contains all the resources to deal with Clients, This request requires a token")
 @RestController
 @RequestMapping("api/v1/clients")
 @RequiredArgsConstructor
@@ -25,6 +32,27 @@ public class ClientController {
     private final ClientService service;
     private final UserService userService;
 
+    @Operation(summary = "create new Client", description = "feature to insert a new client, This request requires a token",
+            responses = {
+                    @ApiResponse(responseCode = "201",
+                            description = "resource created successfully",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ClientResponseDto.class))),
+
+                    @ApiResponse(responseCode = "409",
+                            description = "Client already registered in the system",
+                            content =  @Content(mediaType ="application/json;charset=UTF-8",schema = @Schema(implementation = ErrorMessage.class))),
+
+                    @ApiResponse(responseCode = "422",
+                            description = "resource not processed due to invalid input data",
+                            content =  @Content(mediaType = "application/json;charset=UTF-8",schema = @Schema(implementation = ErrorMessage.class))),
+
+                    @ApiResponse(responseCode = "403",
+                            description = "This feature is not allowed for ADMIN user type",
+                            content =  @Content(mediaType = "application/json;charset=UTF-8",schema = @Schema(implementation = ErrorMessage.class)))
+            }
+
+
+    )
     @PostMapping
     @PreAuthorize("hasRole('CLIENT')")
     public ResponseEntity<ClientResponseDto> insert(@RequestBody @Valid ClientCreateDto dto,
