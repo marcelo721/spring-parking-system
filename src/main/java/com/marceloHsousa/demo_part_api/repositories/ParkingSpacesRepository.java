df7d92bf5112e99c8d4bd92fc617bdaac04319fa
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-06-15 10:28:04 -0300

    implemented getByCode method in ParkingSpacesService class

--- src/main/java/com/marceloHsousa/demo_part_api/repositories/ParkingSpacesRepository.java
+++ src/main/java/com/marceloHsousa/demo_part_api/repositories/ParkingSpacesRepository.java
@@ -3,5 +3,9 @@ package com.marceloHsousa.demo_part_api.repositories;
 import com.marceloHsousa.demo_part_api.entities.ParkingSpaces;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ParkingSpacesRepository extends JpaRepository<ParkingSpaces, Long> {
+
+    Optional<ParkingSpaces> findByCode(String code);
 }
--- src/main/java/com/marceloHsousa/demo_part_api/services/ParkingSpacesService.java
+++ src/main/java/com/marceloHsousa/demo_part_api/services/ParkingSpacesService.java
@@ -3,6 +3,7 @@ package com.marceloHsousa.demo_part_api.services;
 import com.marceloHsousa.demo_part_api.entities.ParkingSpaces;
 import com.marceloHsousa.demo_part_api.repositories.ParkingSpacesRepository;
 import com.marceloHsousa.demo_part_api.services.exceptions.CodeUniqueViolationException;
+import com.marceloHsousa.demo_part_api.services.exceptions.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,12 @@ public class ParkingSpacesService {
         }catch (DataIntegrityViolationException e){
             throw new CodeUniqueViolationException(String.format("ParkingSpace is already registered '%s'", parkingSpaces.getCode()));
         }
+    }
+
+    @Transactional(readOnly = true)
+    public ParkingSpaces getByCode(String code){
+        return repository.findByCode(code).orElseThrow(
+                () -> new EntityNotFoundException("ParkingSpaces not found")
+        );
     }
 }
