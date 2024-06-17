2caab88280b10b9b16fd9c2e7f16deeacf4d7cf5
Marceo Henrique de Sousa <marceloh.sousa@alu.ufc.br>
2024-06-17 14:17:04 -0300

    deleted slq scripts

--- src/test/java/com/marceloHsousa/demo_part_api/resources/sql/users/users-insert.sql
+++
@@ -1,3 +0,0 @@
-insert into users (id, username, password, role) values (100, 'marcelo@gmail.com','123456', 'ROLE_ADMIN');
-insert into users (id, username, password, role) values (101, 'LUCAS@gmail.com','123456', 'ROLE_ADMIN');
-insert into users (id, username, password, role) values (102, 'JOAO@gmail.com','123456', 'ROLE_ADMIN');
